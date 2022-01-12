7b41275b27b50fd98f096b8d505d42010202075b
ajoy267 <andrew.joy12@hotmail.com>
2022-01-11 16:25:12 -0800

    wrote my services todo functions

---
+++ src/services/todo.js
@@ -0,0 +1,16 @@
+import { checkError, client } from './client';
+
+export async function fetchTodoList() {
+  const resp = await client.from('todos').select('*');
+  return checkError(resp);
+}
+
+export async function createTask(task) {
+  const resp = await client.from('todos').insert([{ task: task, user_id: client.auth.user().id }]);
+  return checkError(resp);
+}
+
+export async function deleteTask(id) {
+  const resp = await client.from('todos').delete().match({ id: id });
+  return checkError(resp);
+}
